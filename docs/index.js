7f54f7c2153f9644f3eb8f99b06d5cae320ead59
Chie <chiich@users.noreply.github.com>
2019-01-20 20:47:23 +0000

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,2 +1,17 @@
 Barba.Pjax.Dom.wrapperId = 'pinner';
+
+var HideShowTransition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.finish.bind(this));
+  },
+
+  finish: function() {
+    document.body.scrollTop = 0;
+    this.done();
+  }
+});
+
+Barba.Pjax.getTransition = function() {
+  return HideShowTransition;
+};
 Barba.Pjax.start();
